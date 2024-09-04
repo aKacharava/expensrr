3f3d0609c2312d8a69d7faaafdc56d1e2d3c6683
aKacharava <a.kacharava94@gmail.com>
2024-09-04 17:19:28 +0200

    Adjust app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from models.budget import Budget
+from routes.budget_routes import budget_bp
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Expensrr!'
+# Register Blueprints
+app.register_blueprint(budget_bp)
 
-if __name__ == '__main__':
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
     app.run(debug=True)
