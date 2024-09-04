c1c39e763a90d7198285b90ac3433a06a9e031c1
aKacharava <a.kacharava94@gmail.com>
2024-09-04 17:18:53 +0200

    Add model budget

---
+++ backend/models/budget.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Budget(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
