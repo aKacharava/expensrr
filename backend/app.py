4423aae71847a680e161e01b55fdc55ab4f5c78b
aKacharava <a.kacharava94@gmail.com>
2024-09-05 10:48:30 +0200

    Add test route home

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,13 @@ db.init_app(app)
 # Register Blueprints
 app.register_blueprint(budget_bp)
 
+@app.route('/')
+def hello_world():
+    return 'HELLO Expensrr!'
+
 # Create tables if they don't exist
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
