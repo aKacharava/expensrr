a634bec3ea9cd4cc45adf8cf8d04ef8cc705dd22
Alexander Kacharava <alexander@cube.nl>
2024-09-06 14:12:11 +0200

    Edit column to string

--- backend/models/budget.py
+++ backend/models/budget.py
@@ -4,4 +4,4 @@ class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    date = db.Column(db.String(50), nullable=False)
