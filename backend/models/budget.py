07e780fe0810eaea6c25ac6a8007d4246eebf783
Alexander Kacharava <alexander@cube.nl>
2024-09-06 14:25:27 +0200

    Add to_dict to convert data into dictonary

--- backend/models/budget.py
+++ backend/models/budget.py
@@ -1,7 +1,16 @@
-from db import db  # Import db from db.py
+from db import db
+from datetime import datetime
 
 class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'amount': self.amount,
+            'category': self.category,
+            'date': self.date
+        }
