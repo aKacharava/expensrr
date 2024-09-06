b83f5cb3f0a7e9752449199b0ab910aff82fa2a4
Alexander Kacharava <alexander@cube.nl>
2024-09-06 14:04:10 +0200

    Modify start script

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 echo "Starting backend..."
-if [[ "$OS" == "Windows_NT" ]]; then
-    # Activate the virtual environment on Windows
+if [[ "$OSTYPE" == "msys" ]]; then
+    # Activate the virtual environment on Windows (Git Bash)
     source venv/Scripts/activate
 else
     # Activate the virtual environment on macOS/Linux
     source venv/bin/activate
 fi
+
+# Ensure Flask is run in the correct environment
+export FLASK_APP=app
 flask run
